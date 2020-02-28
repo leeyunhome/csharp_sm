5b38223b0e484c5a34a66375848be465b4dfeb16
leeyunhome <leeyunhome@daum.net>
2020-02-28 19:06:29 +0900

    up to 13_1

---
+++ Chapter13/13_1/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace _13_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBoxLee = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLee)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxLee
+            // 
+            this.pictureBoxLee.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLee.Image")));
+            this.pictureBoxLee.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxLee.Name = "pictureBoxLee";
+            this.pictureBoxLee.Size = new System.Drawing.Size(144, 179);
+            this.pictureBoxLee.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxLee.TabIndex = 0;
+            this.pictureBoxLee.TabStop = false;
+            this.pictureBoxLee.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Gulim", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(159, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Go forward";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBoxLee);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLee)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxLee;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Chapter13/13_1_chart/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13_1_chart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int y = 0;
+            for (int x = -10; x <= 10; x++)
+            {
+                y = (x + 7) * (x + 1) * (x - 5);
+                chart1.Series["Series1"].Points.AddXY(x, y);
+            }
+        }
+    }
+}
